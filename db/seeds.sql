18ebdd5dbe4c50e0119644a91741dd5344568b72
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-05-11 12:35:19 -0600

    seed file

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (id, name)
+VALUES (001, "Human Resources");
+
+INSERT INTO role (id, title, salary, department_id,)
+VALUES (002, "HR MANAGER", 120,000.00, 001);
+
+INSERT INTO employee (id, first_name, last_name, role_id, department_id)
+VALUES (030, "Henrietta", "Ironpants", 002, 007);
+
